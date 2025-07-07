ee87de7cdd01c8ed7df12206cddccd898994790a
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-07-07 18:00:26 +0400

    Pyhton script

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module changes all topics of a document"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes all topics of school document"""
+    mongo_collection.update(
+        { "name": name },
+        { "$set" : { "topics": topics }})
